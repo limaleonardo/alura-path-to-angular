3c98389d08fc84d930c36b11b75ab20e07495735
limaleonardo <lima.leonardo.r@gmail.com>
2021-09-02 19:30:08 -0300

    implementa exemplos da aula 7 e finaliza o curso

--- Project01-ByteBank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ Project01-ByteBank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { Router } from "@angular/router";
 import { Transferencia } from "../models/transferencia.model";
 import { TransferenciaService } from "../services/transferencia.service";
 
@@ -15,7 +16,7 @@ export class NovaTransferenciaComponent{
     valor: number | any;
     destino: number | any;
     
-    constructor(private service: TransferenciaService) {}
+    constructor(private service: TransferenciaService, private router: Router) {}
 
     transferir() {
         const valorEmitir: Transferencia = { valor: this.valor, destino: this.destino };
@@ -23,15 +24,10 @@ export class NovaTransferenciaComponent{
         this.service.adicionar(valorEmitir).subscribe(
           (resultado) => {
             console.log(resultado);
-            this.limparCampos();
+            this.router.navigateByUrl('extrato');
           },
           (error) => console.error(error)
         );
-    }
-
-    limparCampos() {
-        this.valor = 0;
-        this.destino = 0;
     }
 
 }
